701e61cfc8ecd981238f9828efc9b92d614c9ff8
Guido Flohr <guido.flohr@cantanea.com>
2017-09-12 10:08:30 +0300

    new invocation scheme

--- test/textdomain.spec.js
+++ test/textdomain.spec.js
@@ -1,10 +1,14 @@
 var lt = require('../lib/locale-textdomain');
 var chai = require('chai');
 
+chai.expect();
+
+const expect = chai.expect;
+
 describe('When requiring the library', () => {
     describe('and no textdomain was specified', () => {
         it("it should use 'messages'", () => {
-            expect(lt().td().textdomain()).to.be.equal('messages');
+            expect(lt.use().textdomain()).to.be.equal('messages');
         });
     });
 });
