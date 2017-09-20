22d487804374e8eef827d7d91d0464a491947f59
Guido Flohr <guido.flohr@cantanea.com>
2017-09-20 12:30:35 +0300

    test for syntax errors in plural function

--- test/plural-exp.spec.js
+++ test/plural-exp.spec.js
@@ -7,19 +7,34 @@ chai.should();
 
 const should = chai.should;
 
-describe('When specifying an illegal plural function', () => {
-    var plural = pluralExp('alert("Hello, Eve!");'),
-        expect = [
+describe('When specifying an invalid plural function', () => {
+    var expect = [
                     1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
                     1, 1, 1, 1, 1, 1, 1, 1, 1, 1
                  ];
 
-    it("it should use the default function", () => {
-        for (var n = 0; n < expect.length; ++n) {
-            var retval = plural(n);
-            retval[0].should.equal(2);
-            retval[1].should.equal(expect[n]);
-        }
+    describe('that contains illegal code', () => {
+        var plural = pluralExp('alert("Hello, Eve!");');
+
+        it("it should use the default function", () => {
+            for (var n = 0; n < expect.length; ++n) {
+                var retval = plural(n);
+                retval[0].should.equal(2);
+                retval[1].should.equal(expect[n]);
+            }
+        });
+    });
+
+    describe('that contains syntax errors', () => {
+        var plural = pluralExp('rm -rf node_modules && npm install');
+
+        it("it should use the default function", () => {
+            for (var n = 0; n < expect.length; ++n) {
+                var retval = plural(n);
+                retval[0].should.equal(2);
+                retval[1].should.equal(expect[n]);
+            }
+        });
     });
 });
 
