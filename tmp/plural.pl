284992a578cef21744c4aa77d714bf4fc3ae02f8
Lora Tzoneva <lora.tzoneva@cantanea.com>
2017-09-20 15:00:35 +0300

    Romanian test

--- tmp/plural.pl
+++ tmp/plural.pl
@@ -3,7 +3,7 @@
 use strict;
 
 my $function = <<EOF;
-nplurals=3; plural=n==1 ? 0 : n==2 ? 1 : 2;
+nplurals=3; plural=n==1 ? 0 : (n==0 || (n%100 > 0 && n%100 < 20)) ? 1 : 2;
 EOF
 
 $function =~ s/(nplurals|plural|n)/\$$1/g;
