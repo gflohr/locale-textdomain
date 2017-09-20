44023894a54ab3900c88423ddf0e9c711a680699
Guido Flohr <guido.flohr@cantanea.com>
2017-09-20 09:57:44 +0300

    encode retrieved translations

--- src/parse-mo.js
+++ src/parse-mo.js
@@ -22,7 +22,7 @@ function doReadMO(raw) {
 
     blob.skip(4);
     var revision = blob.getUint32();
-    if (revision !== 0) return null;  // Unsupported.
+    if (revision > 1) return null;  // Unsupported.
     var num_strings = blob.getUint32();
     var msgid_offset = blob.getUint32();
     var msgstr_offset = blob.getUint32();
@@ -43,27 +43,52 @@ function doReadMO(raw) {
         trans_tab.push([l, offset]);
     }
 
+    var domain = {};
+    domain.nplurals = 1;
+    domain.pluralFunc = function() { return 0 };
     var messages = {};
-    var encoding = 'utf-8';
+    var poHeader = {};
+    var encoding;
     for (var i = 0; i < num_strings; ++i) {
         var orig = orig_tab[i];
         var l = orig[0];
         var offset = orig[1];
 
         blob.seek(offset);
-        var msgid = blob.getString(l).replace(/\u0000.*/, '');
+        var msgid = blob.getString(l, undefined, encoding).replace(/\u0000.*/, '');
 
         var trans = trans_tab[i];
         l = trans[0];
         offset = trans[1];
 
         blob.seek(offset);
-        var msgstrs = blob.getString(l).split("\u0000");
+        var msgstr = blob.getString(l, undefined, encoding).split("\u0000");
 
-        messages[msgid] = msgstrs;
+        var pairs, kv;
+        if (i === 0 && msgid === '') {
+            pairs = msgstr[0].split('\n');
+            for (var j = 0; j < pairs.length; ++j) {
+                if (pairs[j] !== '') {
+                    kv = pairs[j].split(/[ \t]*:[ \t]*/);
+                    poHeader[kv[0].toLowerCase()] = kv[1];
+                }
+            }
+
+            if (poHeader['content-type'] !== undefined) {
+                var enc = poHeader['content-type'].replace(/.*=/, '');
+                if (enc !== poHeader['content-type']
+                    && iconv.encodingExists(enc)) {
+                    poHeader.charset = encoding = enc;
+                }
+            }
+        }
+
+        messages[msgid] = msgstr;
     }
 
-    return messages;
+    domain.messages = messages;
+
+    return domain; 
 }
 
 module.exports = readMO;
